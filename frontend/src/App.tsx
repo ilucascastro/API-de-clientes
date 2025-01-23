8acec80bb63587c184d8d3b6f1a52471db2102ad
Lucas <lucascastro.mao@gmail.com>
2025-01-23 17:43:37 -0400

    Primeiro commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+export default function App(){
+  return (
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+      <main className="my-10 w-full md: max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">
+          Clientes
+        </h1>
+        <form className="flex flex-col my-6">
+          <label className="font-medium text-white">Nome:</label>
+          <input type="text" placeholder="Digite seu nome completo...">
+          </input>
+        </form>
+      </main>
+    </div>
+  )
+}
